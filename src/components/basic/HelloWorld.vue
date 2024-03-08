a5209400d3e850c65b542150e49b4061251a89b7
Rachele Bizzarri <rachebizz@gmail.com>
2024-03-08 21:31:34 +0100

    fix: fixed text

--- src/components/basic/HelloWorld.vue
+++ src/components/basic/HelloWorld.vue
@@ -17,7 +17,7 @@ export default defineComponent({
             <p class="paragraph-1 mt-3">
                 I was born on August the 4th 1994 in Volterra, where I graduated in accounting with a curriculim in programming, at Istituto Tecnico Commerciale F. Niccolini. Afterwards, in 2013, I moved to Pisa to start my study at Università di Pisa where I've erned a master's degree in <span class="highlight">Digital Humanities</span> in 2019.
             </p>       
-            <p class="paragraph-1 mt-1"> I really like to read and my greatest passion is music.</p>
+            <p class="paragraph-1 mt-1">I really like to read and my greatest passion is music.</p>
             <p class="paragraph-1 mt-1">I'm particularly interested in the potential of digital technologies as a powerful inclusive and democratizing tool for dissemination on cultural and artistic heritage.</p>
             <p class="paragraph-1 mt-5">At the time, I'm working at MONOGRID.</p>
         </div>
