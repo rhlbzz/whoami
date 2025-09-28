a6d288af245a3888eac74648ef0482e983bc24a4
Rachele Bizzarri <rachebizz@gmail.com>
2025-09-28 17:50:31 +0200

    feat: add new self image

--- src/components/basic/IntroSection.vue
+++ src/components/basic/IntroSection.vue
@@ -1,13 +1,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import EmojiWave from '@/assets/img/emoji-wave-01.png?url'
+import SelfImg from '@/assets/img/self.png?url'
 
 export default defineComponent({
     name: 'IntroSection',
     setup() {
 
       return {
-        EmojiWave
+        SelfImg
       }
     }
 })
@@ -15,16 +15,14 @@ export default defineComponent({
 
 <template>
   <!-- <section class="wrapper flex flex-col justify-center items-start"> -->
-  <section class="wrapper overflow-hidden intro-safe-area flex justify-center items-center flex-col lg:flex-row">
-    <div class="flex flex-col column-01 justify-center items-center text-center lg:text-left lg:items-start lg:w-[80%]">
+  <section class="wrapper overflow-hidden intro-safe-area flex items-center flex-col lg:justify-center lg:flex-row">
+    <div class="flex flex-col items-center text-center mt-[20%] lg:mt-0 lg:text-left lg:items-end">
       <h2 class="head-4">Hi there! I'm</h2>
       <h1 class="head-1">Rachele Bizzarri</h1>
       <h2 class="head-4">and I am a</h2>
-      <h2 class="head-2">web developer.</h2>
+      <h2 class="head-2">web developer</h2>
     </div>
-    <div class="flex column-02 w-[35%] mt-[2%] lg:w-[20%] lg:mt-[0]">
-      <img :src="EmojiWave" class="w-full" />
-    </div>
+    <img :src="SelfImg" class="mt-4 w-[80%] lg:mt-0 lg:w-[auto] h-[auto]" />
   </section>
 </template>
 
