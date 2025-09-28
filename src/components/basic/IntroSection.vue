efd9958a4e0c79595bdeec16435cfb06a5b9c51c
Rachele Bizzarri <rachebizz@gmail.com>
2025-09-28 11:22:46 +0200

    feat: update texts

--- src/components/basic/IntroSection.vue
+++ src/components/basic/IntroSection.vue
@@ -20,7 +20,7 @@ export default defineComponent({
       <h2 class="head-4">Hi there! I'm</h2>
       <h1 class="head-1">Rachele Bizzarri</h1>
       <h2 class="head-4">and I am a</h2>
-      <h2 class="head-2">front-end developer.</h2>
+      <h2 class="head-2">web developer.</h2>
     </div>
     <div class="flex column-02 w-[35%] mt-[2%] lg:w-[20%] lg:mt-[0]">
       <img :src="EmojiWave" class="w-full" />
